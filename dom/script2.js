579b63e963514ce834026dcc27913d4e190180c2
DclFabienLycan <dcl.fabienb@18pixel.fr>
2018-10-22 17:05:09 +0200

    update

--- dom/script2.js
+++ dom/script2.js
@@ -4,6 +4,9 @@ var button2 = document.getElementById("button2");
 var button3 = document.getElementById("button3");
 var button4 = document.getElementById("button4");
 var button5 = document.getElementById("button5");
+var button6 = document.getElementById("button6");
+// rajouter une variable pour incrémentation de 50 px
+var growUp = 100; 
 // var isActivated = false;
 
 button1.addEventListener ("click", 
@@ -21,6 +24,7 @@ function () {
 button3.addEventListener ("click", 
 function () {
     carre.style.width = "300px";
+}   setTimeout () {
     carre.style.height = "300px";
 }
 );
@@ -35,4 +39,23 @@ button5.addEventListener ("click",
 function () {
     carre.style = "none";
 }
-)
+);
+
+// Faire une condition liée à l'incrémentation des 50 px
+// augmente de 50px par clic
+// arrivé à 500px, reset.
+button6.addEventListener("click", 
+function gUp () {
+    if (growUp <= 500) {
+        // incrémenter de 50px
+        document.getElementById("carre").style.width = growUp + "px";
+        document.getElementById("carre").style.height = growUp + "px";
+        growUp = growUp + 50;
+    } else {
+        // retourner a 100px
+        document.getElementById("carre").style.width = growUp +"px";
+        document.getElementById("carre").style.height = growUp +"px";
+        growUp = 100;
+    }
+}
+);
